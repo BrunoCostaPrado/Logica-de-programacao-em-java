1719803bad64cc8a9e53ccf0b992ec8f1cc20c11
BrunoCostaPrado <brunocostaprado@gmail.com>
2021-11-14 21:35:57 -0300

    ,.

--- ex21.js
+++ ex21.js
@@ -1,8 +1,9 @@
-var hi = +(prompt("Digite o horario de inicio"));
-var hf = +(prompt("Digite o horario de termino"));
-var ht = (hf - hi)
+var hi = +prompt("Digite o horario de inicio");
+var hf = +prompt("Digite o horario de termino");
+var ht = hf - hi;
 if (hi > hf) {
-    alert("Total de horas foi:" + ht)
+  alert("Total de horas foi de " + ht + " horas");
 } else {
-    alert("Total de horas foi:" + ht)
+  ht = ht + 24;
+  alert("Total de horas foi de " + ht + " horas");
 }
